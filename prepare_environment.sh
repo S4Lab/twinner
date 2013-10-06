8102640ea8a3039f60730c88792b83515e60b58c
Behnam Momeni <sbmomeni@gmail.com>
2013-10-06 11:27:46 +0330

    The preparation script gets "full" for setting /proc entries

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,12 +1,18 @@
+fullpreparation=$1
+
 cp -l -a -u src/* "deploy/"
-ptracescope=$(cat /proc/sys/kernel/yama/ptrace_scope)
-aslrstate=$(cat /proc/sys/kernel/randomize_va_space)
-if [ ! "$ptracescope$aslrstate" = "00" ]; then
-	if [ ! "$ptracescope" = "0" ]; then
-		echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope > /dev/null
+
+if [ "a$fullpreparation" = "afull" ]; then
+	ptracescope=$(cat /proc/sys/kernel/yama/ptrace_scope)
+	aslrstate=$(cat /proc/sys/kernel/randomize_va_space)
+	if [ ! "$ptracescope$aslrstate" = "00" ]; then
+		if [ ! "$ptracescope" = "0" ]; then
+			echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope > /dev/null
+		fi
+		if [ ! "$aslrstate" = "0" ]; then
+			echo 0 | sudo tee /proc/sys/kernel/randomize_va_space > /dev/null
+		fi
+		sudo -k
 	fi
-	if [ ! "$aslrstate" = "0" ]; then
-		echo 0 | sudo tee /proc/sys/kernel/randomize_va_space > /dev/null
-	fi
-	sudo -k
 fi
+
