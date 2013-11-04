655a4ff4923bca15bbeabe8da48754a962c6d1ea
Behnam Momeni <sbmomeni@gmail.com>
2013-11-04 10:42:16 +0330

    Disabling swap during initial preparation to avoid crash on ram outrun

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,20 +1,27 @@
 fullpreparation=$1
 
+echo "Linking to source files from deploy folder..."
 cp -l -a -u src/* "deploy/"
 cp -l -a -u test/simple-condition-test.out deploy/obj-intel64/
 
 if [ "a$fullpreparation" = "afull" ]; then
+	echo "Full preparation..."
 	ptracescope=$(cat /proc/sys/kernel/yama/ptrace_scope)
 	aslrstate=$(cat /proc/sys/kernel/randomize_va_space)
 	if [ ! "$ptracescope$aslrstate" = "00" ]; then
 		if [ ! "$ptracescope" = "0" ]; then
+			echo "Disabling yama ptrace protection for parent-child debugging..."
 			echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope > /dev/null
 		fi
 		if [ ! "$aslrstate" = "0" ]; then
+			echo "Disabling ASLR for multiple executions..."
 			echo 0 | sudo tee /proc/sys/kernel/randomize_va_space > /dev/null
 		fi
+		echo "Disabling swap for memory loop safty..."
+		swapoff -a
 		sudo -k
 	fi
 	mkdir -p /tmp/twinner
 fi
+echo "Done."
 
