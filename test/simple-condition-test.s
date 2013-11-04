8c238812432b326252fdde5a39ad9ea1483dd013
Behnam Momeni <sbmomeni@gmail.com>
2013-11-04 10:43:25 +0330

    Adding debugging symbols to assembly simple test program

--- test/simple-condition-test.s
+++ test/simple-condition-test.s
@@ -1,9 +1,12 @@
 	.file	"simple-condition-test.c"
 	.text
+.Ltext0:
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
+	.file 1 "simple-condition-test.c"
+	.loc 1 3 0
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -12,6 +15,7 @@ main:
 	.cfi_def_cfa_register 6
 	movl	%edi, -20(%rbp)
 	movq	%rsi, -32(%rbp)
+	.loc 1 4 0
 	movq	-32(%rbp), %rax
 	addq	$8, %rax
 	movq	(%rax), %rax
@@ -19,6 +23,7 @@ main:
 	movsbl	%al, %eax
 	subl	$48, %eax
 	movl	%eax, -4(%rbp)
+	.loc 1 5 0
 	movq	-32(%rbp), %rax
 	addq	$16, %rax
 	movq	(%rax), %rax
@@ -26,28 +31,311 @@ main:
 	movsbl	%al, %eax
 	subl	$48, %eax
 	movl	%eax, -8(%rbp)
+	.loc 1 6 0
 	movl	-4(%rbp), %eax
 	addl	%eax, %eax
 	addl	$3, %eax
 	movl	%eax, -12(%rbp)
+	.loc 1 7 0
 	movl	-8(%rbp), %eax
 	subl	$3, %eax
 	movl	%eax, -16(%rbp)
+	.loc 1 8 0
 	movl	-16(%rbp), %eax
 	addl	$4, %eax
 	cmpl	-12(%rbp), %eax
 	jne	.L2
+	.loc 1 9 0
 	movl	$0, -12(%rbp)
 	jmp	.L3
 .L2:
+	.loc 1 11 0
 	movl	$32, -12(%rbp)
 .L3:
+	.loc 1 13 0
 	movl	$0, %eax
+	.loc 1 14 0
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (GNU) 4.8.1 20130725 (prerelease)"
+.Letext0:
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.long	0xea
+	.value	0x4
+	.long	.Ldebug_abbrev0
+	.byte	0x8
+	.uleb128 0x1
+	.long	.LASF11
+	.byte	0x1
+	.long	.LASF12
+	.long	.LASF13
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.long	.Ldebug_line0
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x7
+	.long	.LASF0
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x8
+	.long	.LASF1
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x7
+	.long	.LASF2
+	.uleb128 0x2
+	.byte	0x4
+	.byte	0x7
+	.long	.LASF3
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF4
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x5
+	.long	.LASF5
+	.uleb128 0x3
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x5
+	.long	.LASF6
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x7
+	.long	.LASF7
+	.uleb128 0x4
+	.byte	0x8
+	.long	0x72
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF8
+	.uleb128 0x5
+	.long	.LASF14
+	.byte	0x1
+	.byte	0x3
+	.long	0x57
+	.quad	.LFB0
+	.quad	.LFE0-.LFB0
+	.uleb128 0x1
+	.byte	0x9c
+	.long	0xe7
+	.uleb128 0x6
+	.long	.LASF9
+	.byte	0x1
+	.byte	0x3
+	.long	0x57
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -36
+	.uleb128 0x6
+	.long	.LASF10
+	.byte	0x1
+	.byte	0x3
+	.long	0xe7
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -48
+	.uleb128 0x7
+	.string	"a"
+	.byte	0x1
+	.byte	0x4
+	.long	0x57
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -20
+	.uleb128 0x7
+	.string	"b"
+	.byte	0x1
+	.byte	0x5
+	.long	0x57
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -24
+	.uleb128 0x7
+	.string	"c"
+	.byte	0x1
+	.byte	0x6
+	.long	0x57
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -28
+	.uleb128 0x7
+	.string	"d"
+	.byte	0x1
+	.byte	0x7
+	.long	0x57
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -32
+	.byte	0
+	.uleb128 0x4
+	.byte	0x8
+	.long	0x6c
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x1b
+	.uleb128 0xe
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x7
+	.uleb128 0x10
+	.uleb128 0x17
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.byte	0
+	.byte	0
+	.uleb128 0x4
+	.uleb128 0xf
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x5
+	.uleb128 0x2e
+	.byte	0x1
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x27
+	.uleb128 0x19
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x7
+	.uleb128 0x40
+	.uleb128 0x18
+	.uleb128 0x2117
+	.uleb128 0x19
+	.uleb128 0x1
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x6
+	.uleb128 0x5
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2
+	.uleb128 0x18
+	.byte	0
+	.byte	0
+	.uleb128 0x7
+	.uleb128 0x34
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x8
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2
+	.uleb128 0x18
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.long	0x2c
+	.value	0x2
+	.long	.Ldebug_info0
+	.byte	0x8
+	.byte	0
+	.value	0
+	.value	0
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.quad	0
+	.quad	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF11:
+	.string	"GNU C 4.8.2 -mtune=generic -march=x86-64 -g"
+.LASF3:
+	.string	"unsigned int"
+.LASF4:
+	.string	"signed char"
+.LASF13:
+	.string	"/home/behnam/thesis/tools/Twinner/test"
+.LASF12:
+	.string	"simple-condition-test.c"
+.LASF0:
+	.string	"long unsigned int"
+.LASF2:
+	.string	"short unsigned int"
+.LASF8:
+	.string	"char"
+.LASF1:
+	.string	"unsigned char"
+.LASF14:
+	.string	"main"
+.LASF6:
+	.string	"long int"
+.LASF9:
+	.string	"argc"
+.LASF5:
+	.string	"short int"
+.LASF7:
+	.string	"sizetype"
+.LASF10:
+	.string	"argv"
+	.ident	"GCC: (GNU) 4.8.2"
 	.section	.note.GNU-stack,"",@progbits
