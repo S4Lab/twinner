43f0b6765d8c76cb73744b2d6b7567cd2b02b111
Behnam Momeni <sbmomeni@gmail.com>
2013-11-28 16:57:27 +0330

    Changing the 7 steps twin generation algorithm to make it realizable

--- src/edu/sharif/twinner/engine/Twinner.cpp
+++ src/edu/sharif/twinner/engine/Twinner.cpp
@@ -87,32 +87,24 @@ void Twinner::setInputBinaryArguments (string arguments) {
 /**
  * Generates twin code and twin binary.
  * Steps:
- * 1. Without any special initialization (just giving as much arguments as possible,
- * to the program; indeed we do not need to reach even near max limits, just we must be
- * sure to use a lot of bytes to find places whom are used by user input to ignore
- * initializing them while generating the twin code), run program and obtain first trace,
- * 2. With completely different arguments, run program in change-detection mode for the
- * second trace,
- * 3. Find all symbols who change their initial values and mark them as user inputs (do
- * not store 2nd trace as it has no info else of position of user inputs),
- * 4. While generating twin code, initialize all symbols who were visited in first trace
- * segment, else of user inputs (user inputs and next trace segments' symbols remain
- * uninitialized to obtain and keep their values from user and syscalls),
- * 5. Since now (3rd execution and so on) use DFS or another search strategy to obtain a
- * set of new values for symbols to follow a new execution path and run program in
- * initialized mode (which despite change-detection mode, enforces symbols values)
- * with those symbols' values (initializing user inputs is not required when generating
- * twin code, but they should be initialized while running program as different execution
- * traces may be obtained with different user inputs),
- * 6. By each run, some new (surely non-user-input) symbols may be added to first
- * segment. If there was a symbol (at an address) in 2nd or later segments while there was
- * no symbols at that address in 1st segment, it should be added to first segment symbols
- * too (to have it initialized in the final twin code by its pre-syscall value),
- * 7. To obtain step-6's values, run program again (just one time for all symbols who are
- * tracked) at the end in initial-state-detection mode and look for values of those
- * symbols before first syscall. It's possible that a later trace, removes a symbol from
- * the set of non-initialized-in-1st-segment symbols. So search for those symbols just at
- * the end and then do that last/final run to look for their pre-1st-syscall values.
+ * 1. With given arguments, run the program and collect its trace, updating the CFG,
+ * 2. Solve constraints of last trace, traversing the CFG (e.g. using DFS), and find
+ * a set of symbols' values which enforce a new execution path. While finding this set
+ * of values, only symbols which are participating in corresponding constraints will
+ * be considered. Thus all valued symbols will be seen by TwinTool in next run for sure
+ * and the expected path will be traversed (of course some of old visited symbols, which
+ * were ignored as they were not participating in the newly followed path of execution,
+ * may be revisited in the next run),
+ * 3. Continue collecting new traces by repeating step 2 until there was no remaining
+ * untraversed execution path in the discovered CFG,
+ * 4. At the end, all addresses which were holding some memory emerged symbol (found by
+ * looking in the registers/memory addresses/constraints expressions) should be collected
+ * as candidates to be initialized at the beginning of the Twin code,
+ * 5. Pass all of step 4's set of addresses and collect their initial values before the
+ * first syscall,
+ * 6. Change argument of the program and repeat step 5 with new arguments,
+ * 7. Remove all addresses who have different values between two runs of steps 5 and 6
+ * and initialize remaining addresses in the Twin code.
  */
 void Twinner::generateTwinBinary () {
   Executer ex (pin, twintool, input, arguments);
