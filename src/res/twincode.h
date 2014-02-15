fcf7a8cca4ebc381b31bb581fca36518589942d5
Behnam Momeni <b_momeni@ce.sharif.edu>
2014-02-15 14:46:47 +0330

    Setting/Restoring registers values while invoking syscall

--- src/res/twincode.h
+++ src/res/twincode.h
@@ -33,25 +33,67 @@ struct RegistersSet {
   UINT64 r15;
 };
 
+#define SAVING_REGISTERS_COMMANDS(REGID) \
+    "movq   %%rax,  %"      #REGID "\n\t" \
+    "movq   %%rbx,  8+%"    #REGID "\n\t" \
+    "movq   %%rcx,  16+%"   #REGID "\n\t" \
+    "movq   %%rdx,  24+%"   #REGID "\n\t" \
+    "movq   %%rdi,  32+%"   #REGID "\n\t" \
+    "movq   %%rsi,  40+%"   #REGID "\n\t" \
+    "movq   %%rsp,  48+%"   #REGID "\n\t" \
+    "movq   %%rbp,  56+%"   #REGID "\n\t" \
+    "movq   %%r8,   64+%"   #REGID "\n\t" \
+    "movq   %%r9,   72+%"   #REGID "\n\t" \
+    "movq   %%r10,  80+%"   #REGID "\n\t" \
+    "movq   %%r11,  88+%"   #REGID "\n\t" \
+    "movq   %%r12,  96+%"   #REGID "\n\t" \
+    "movq   %%r13,  104+%"  #REGID "\n\t" \
+    "movq   %%r14,  112+%"  #REGID "\n\t" \
+    "movq   %%r15,  120+%"  #REGID "\n\t"
+
+#define LOADING_REGISTERS_COMMANDS(REGID) \
+    "movq   64+%"   #REGID ", %%r8\n\t" \
+    "movq   72+%"   #REGID ", %%r9\n\t" \
+    "movq   80+%"   #REGID ", %%r10\n\t" \
+    "movq   88+%"   #REGID ", %%r11\n\t" \
+    "movq   96+%"   #REGID ", %%r12\n\t" \
+    "movq   104+%"  #REGID ", %%r13\n\t" \
+    "movq   112+%"  #REGID ", %%r14\n\t" \
+    "movq   120+%"  #REGID ", %%r15\n\t" \
+    "movq   %"      #REGID ", %%rax\n\t" \
+    "movq   8+%"    #REGID ", %%rbx\n\t" \
+    "movq   16+%"   #REGID ", %%rcx\n\t" \
+    "movq   24+%"   #REGID ", %%rdx\n\t" \
+    "movq   32+%"   #REGID ", %%rdi\n\t" \
+    "movq   40+%"   #REGID ", %%rsi\n\t" \
+    "movq   48+%"   #REGID ", %%rsp\n\t" \
+    "movq   56+%"   #REGID ", %%rbp\n\t"
+
 #define SAVE_REGISTERS(REGS) \
   asm ( \
-    "movq   %%rax,  %0\n\t" \
-    "movq   %%rbx,  8+%0\n\t" \
-    "movq   %%rcx,  16+%0\n\t" \
-    "movq   %%rdx,  24+%0\n\t" \
-    "movq   %%rdi,  32+%0\n\t" \
-    "movq   %%rsi,  40+%0\n\t" \
-    "movq   %%rsp,  48+%0\n\t" \
-    "movq   %%rbp,  56+%0\n\t" \
-    "movq   %%r8,   64+%0\n\t" \
-    "movq   %%r9,   72+%0\n\t" \
-    "movq   %%r10,  80+%0\n\t" \
-    "movq   %%r11,  88+%0\n\t" \
-    "movq   %%r12,  96+%0\n\t" \
-    "movq   %%r13,  104+%0\n\t" \
-    "movq   %%r14,  112+%0\n\t" \
-    "movq   %%r15,  120+%0\n\t" \
+    SAVING_REGISTERS_COMMANDS (0) \
     : "=o" (REGS) \
   )
+
+RegistersSet setRegistersValuesAndInvokeSyscall (RegistersSet regs) {
+  static RegistersSet currentRegs; // static => to be coded independent of rsp and so on
+  SAVE_REGISTERS (currentRegs);
+  static RegistersSet resultingRegs;
+  asm (
+        /* loading registers' values from regs (the %1) variable */
+        LOADING_REGISTERS_COMMANDS (1)
+        /* the syscall itself */
+        "syscall" /* FIXME: the syscall code should specify its args/aux-info */
+        /* saving resulting registers' values within resultingRegs (the %0) variable */
+        SAVING_REGISTERS_COMMANDS (0)
+        /* loading registers' old values from currentRegs (the %2) variable */
+        LOADING_REGISTERS_COMMANDS (2)
+
+        : "=o" (resultingRegs)
+        : "o" (regs), "o" (currentRegs)
+        : "cc" /* FIXME: check whether syscall requires clobbering flags or not */
+        );
+  return resultingRegs;
+}
 
 #endif	/* twincode.h */
