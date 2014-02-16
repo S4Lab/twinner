fda781f8c411809370d72dbc2a42ac70ac20d243
Behnam Momeni <b_momeni@ce.sharif.edu>
2014-02-16 13:53:43 +0330

    bumping copyright year (due to new release)

--- src/edu/sharif/twinner/cli/main.cpp
+++ src/edu/sharif/twinner/cli/main.cpp
@@ -1,7 +1,7 @@
 //                   In the name of GOD
 /*
  * Twinner: An unpacker which utilizes concolic execution.
- * Copyright © 2013  Behnam Momeni
+ * Copyright © 2013-2014  Behnam Momeni
  *
  * This program comes with ABSOLUTELY NO WARRANTY.
  * See the COPYING file distributed with this work for information 
@@ -170,7 +170,7 @@ void printVersion () {
 
 void printHelp (string progName, const ArgParser::Option options[]) {
   cout << "Twinner: An unpacker which utilizes concolic execution." << endl;
-  cout << "Copyright © 2013  Behnam Momeni" << endl;
+  cout << "Copyright © 2013-2014  Behnam Momeni" << endl;
   cout << endl;
   cout << "Usage: " << progName << " [options]" << endl;
   cout << "Options:" << endl;
@@ -194,7 +194,7 @@ void printHelp (string progName, const ArgParser::Option options[]) {
 
 void printLicense () {
   cout << "Twinner: An unpacker which utilizes concolic execution." << endl;
-  cout << "Copyright © 2013  Behnam Momeni" << endl;
+  cout << "Copyright © 2013-2014  Behnam Momeni" << endl;
   cout << endl;
   cout << "Not implemented yet!!!! See COPYING file instead." << endl;
 }
