bb3c9dec096bdfa314e0ba15d6fece5da8e8ea3a
Behnam Momeni <b_momeni@ce.sharif.edu>
2014-02-26 10:30:15 +0330

    Emptying temp folder before execution of twinner

--- run_twinner.sh
+++ run_twinner.sh
@@ -1,1 +1,2 @@
+sudo su sandbox -c "rm -f /tmp/twinner/*"
 sudo su sandbox -c "./obj-intel64/Twinner --input ./obj-intel64/multi-path-with-printf-leafs-test.out --args '475' --pin-launcher ../../../pin.sh --tool ./obj-intel64/TwinTool.so -o /tmp/twinner/unpacked.twin.c --verbose $@"
