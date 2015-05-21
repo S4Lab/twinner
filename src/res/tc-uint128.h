3b704430b5c2df4c3929a221491fe58208bbe4db
Behnam Momeni <b_momeni@ce.sharif.edu>
2015-05-21 10:02:18 +0430

    Converting UINT128 to an struct + adding more 128-operations

--- src/res/tc-uint128.h
+++ src/res/tc-uint128.h
@@ -13,238 +13,251 @@
 #ifndef TC_UINT128_H
 #define TC_UINT128_H
 
-class UINT128 {
+struct UINT128 {
+  UINT64 low;
+  UINT64 high;
 
-private:
-  UINT64 high, low;
-
-public:
-
-  constexpr UINT128 (UINT64 _high, UINT64 _low)
-      : high (_high), low (_low) {
+  constexpr UINT128 (const UINT64 _high, const UINT64 _low)
+  : high (_high), low (_low) {
   }
 
   constexpr UINT128 (const UINT32 a, const UINT32 b, const UINT32 c, const UINT32 d)
-      : high ((UINT64 (a) << 32) | b), low ((UINT64 (c) << 32) | d) {
+  : high ((UINT64 (a) << 32) | b), low ((UINT64 (c) << 32) | d) {
   }
 
   constexpr UINT128 (const UINT64 *v)
-      : low (v[0]), high (v[1]) {
+  : low (v[0]), high (v[1]) {
   }
 
-  bool operator== (const UINT128 &v) const {
-    return high == v.high && low == v.low;
-  }
-
-  bool operator== (const UINT64 &v) const {
-    return high == 0 && low == v;
-  }
-
-  bool operator!= (const UINT128 &v) const {
-    return !((*this) == v);
-  }
-
-  bool operator!= (const UINT64 &v) const {
-    return !((*this) == v);
-  }
-
-  bool operator< (const UINT128 &v) const {
-    return high < v.high || (high == v.high && low < v.low);
-  }
-
-  bool operator< (const UINT64 &v) const {
-    return high == 0 && low < v;
-  }
-
-  bool operator<= (const UINT128 &v) const {
-    return high < v.high || (high == v.high && low <= v.low);
-  }
-
-  bool operator<= (const UINT64 &v) const {
-    return high == 0 && low <= v;
-  }
-
-  bool operator> (const UINT128 &v) const {
-    return !((*this) <= v);
-  }
-
-  bool operator> (const UINT64 &v) const {
-    return !((*this) <= v);
-  }
-
-  bool operator>= (const UINT128 &v) const {
-    return !((*this) < v);
-  }
-
-  bool operator>= (const UINT64 &v) const {
-    return !((*this) < v);
-  }
-
-  UINT64 toUint64 () const {
+  constexpr UINT64 toUint64 () const {
     return low;
   }
 
-  UINT128 operator^ (const UINT128 &v) const {
-    return UINT128 (high ^ v.high, low ^ v.low);
-  }
-
-  UINT128 operator& (const UINT128 &v) const {
-    return UINT128 (high & v.high, low & v.low);
-  }
-
-  UINT64 operator& (const UINT64 &v) const {
-    return low & v;
-  }
-
-  UINT128 operator| (const UINT128 &v) const {
-    return UINT128 (high | v.high, low | v.low);
-  }
-
-  UINT128 operator| (const UINT64 &v) const {
-    return UINT128 (high, low | v);
-  }
-
-  UINT128 operator>> (int bits) const {
-    if (bits < 64) {
-      return UINT128 (high >> bits, (high << (64 - bits)) | (low >> bits));
-    } else {
-      return UINT128 (0, high >> (bits - 64));
-    }
-  }
-
-  UINT128 operator<< (int bits) const {
-    if (bits < 64) {
-      return UINT128 ((high << bits) | (low >> (64 - bits)), low << bits);
-    } else {
-      return UINT128 (low << (bits - 64), 0);
-    }
-  }
-
-  UINT64 operator/ (UINT64 divisor) const {
-    UINT64 quotient;
-    asm (
-          "DIV %[divisor]"
-          : "=a" (quotient)
-          : "d" (high), "a" (low), [divisor] "r" (divisor)
-          );
-    return quotient;
-  }
-
-  UINT128 operator% (const UINT128 &divisor) const {
-    if (divisor.high == 0) {
-      return UINT128 (0, (*this) % (divisor.low));
-    }
-    UINT128 remainder = *this;
-    while (remainder >= divisor) {
-      remainder = remainder - divisor;
-    }
-    return remainder;
-  }
-
-  UINT64 operator% (UINT64 divisor) const {
-    UINT64 remainder;
-    asm (
-          "DIV %[divisor]"
-          : "=d" (remainder)
-          : "d" (high), "a" (low), [divisor] "r" (divisor)
-          );
-    return remainder;
-  }
-
-  UINT128 operator+ (UINT64 addee) const {
-    UINT64 msp, lsp;
-    asm (
-          "ADD %[a], %%rbx\n\t"
-          "ADC $0, %%rcx"
-          : "=c" (msp), "=b" (lsp)
-          : "c" (high), "b" (low), [a] "r" (addee)
-          );
-    return UINT128 (msp, lsp);
-  }
-
-  UINT128 operator+ (UINT128 addee) const {
-    UINT64 msp, lsp;
-    asm (
-          "ADD %[l], %%rbx\n\t"
-          "ADC %[h], %%rcx"
-          : "=c" (msp), "=b" (lsp)
-          : "c" (high), "b" (low), [h] "r" (addee.high), [l] "r" (addee.low)
-          );
-    return UINT128 (msp, lsp);
-  }
-
-  UINT128 operator- (UINT64 minusee) const {
-    UINT64 msp, lsp;
-    asm (
-          "SUB %[a], %%rbx\n\t"
-          "SBB $0, %%rcx"
-          : "=c" (msp), "=b" (lsp)
-          : "c" (high), "b" (low), [a] "r" (minusee)
-          );
-    return UINT128 (msp, lsp);
-  }
-
-  UINT128 operator- (UINT128 minusee) const {
-    UINT64 msp, lsp;
-    asm (
-          "SUB %[l], %%rbx\n\t"
-          "SBB %[h], %%rcx"
-          : "=c" (msp), "=b" (lsp)
-          : "c" (high), "b" (low), [h] "r" (minusee.high), [l] "r" (minusee.low)
-          );
-    return UINT128 (msp, lsp);
-  }
-
-  UINT128 operator- () const {
-    return UINT128 (0, 0) - (*this);
-  }
-
-  friend inline UINT128 arithmeticShiftToRight (const UINT128 &v, int bits) {
-    return UINT128 (INT64 (v.high) >> bits, (v.high << (64 - bits)) | (v.low >> bits));
+  constexpr explicit operator UINT64 () const {
+    return toUint64 ();
   }
 };
 
-static inline bool operator== (const UINT64 &a, const UINT128 &b) {
+bool operator== (const UINT128 u, const UINT128 v) {
+  return u.high == v.high && u.low == v.low;
+}
+
+bool operator== (const UINT128 u, const UINT64 v) {
+  return u.high == 0 && u.low == v;
+}
+
+bool operator!= (const UINT128 u, const UINT128 v) {
+  return !(u == v);
+}
+
+bool operator!= (const UINT128 u, const UINT64 v) {
+  return !(u == v);
+}
+
+bool operator< (const UINT128 u, const UINT128 v) {
+  return u.high < v.high || (u.high == v.high && u.low < v.low);
+}
+
+bool operator< (const UINT128 u, const UINT64 v) {
+  return u.high == 0 && u.low < v;
+}
+
+bool operator<= (const UINT128 u, const UINT128 v) {
+  return u.high < v.high || (u.high == v.high && u.low <= v.low);
+}
+
+bool operator<= (const UINT128 u, const UINT64 v) {
+  return u.high == 0 && u.low <= v;
+}
+
+bool operator> (const UINT128 u, const UINT128 v) {
+  return !(u <= v);
+}
+
+bool operator> (const UINT128 u, const UINT64 v) {
+  return !(u <= v);
+}
+
+bool operator>= (const UINT128 u, const UINT128 v) {
+  return !(u < v);
+}
+
+bool operator>= (const UINT128 u, const UINT64 v) {
+  return !(u < v);
+}
+
+UINT128 operator^ (const UINT128 u, const UINT128 v) {
+  return UINT128 (u.high ^ v.high, u.low ^ v.low);
+}
+
+UINT128 operator& (const UINT128 u, const UINT128 v) {
+  return UINT128 (u.high & v.high, u.low & v.low);
+}
+
+UINT64 operator& (const UINT128 u, const UINT64 v) {
+  return u.low & v;
+}
+
+UINT128 operator| (const UINT128 u, const UINT128 v) {
+  return UINT128 (u.high | v.high, u.low | v.low);
+}
+
+UINT128 operator| (const UINT128 u, const UINT64 v) {
+  return UINT128 (u.high, u.low | v);
+}
+
+UINT128 operator| (const UINT64 v, const UINT128 u) {
+  return UINT128 (u.high, u.low | v);
+}
+
+UINT128 operator>> (const UINT128 u, const int bits) {
+  if (bits < 64) {
+    return UINT128 (u.high >> bits, (u.high << (64 - bits)) | (u.low >> bits));
+  } else {
+    return UINT128 (0, u.high >> (bits - 64));
+  }
+}
+
+UINT128 operator<< (const UINT128 u, const int bits) {
+  if (bits < 64) {
+    return UINT128 ((u.high << bits) | (u.low >> (64 - bits)), u.low << bits);
+  } else {
+    return UINT128 (u.low << (bits - 64), 0);
+  }
+}
+
+UINT128 operator+ (const UINT128 u, const UINT64 addee) {
+  UINT64 msp, lsp;
+  asm (
+        "ADD %[a], %%rbx\n\t"
+        "ADC $0, %%rcx"
+        : "=c" (msp), "=b" (lsp)
+        : "c" (u.high), "b" (u.low), [a] "r" (addee)
+        );
+  return UINT128 (msp, lsp);
+}
+
+UINT128 operator+ (const UINT128 u, const UINT128 addee) {
+  UINT64 msp, lsp;
+  asm (
+        "ADD %[l], %%rbx\n\t"
+        "ADC %[h], %%rcx"
+        : "=c" (msp), "=b" (lsp)
+        : "c" (u.high), "b" (u.low), [h] "r" (addee.high), [l] "r" (addee.low)
+        );
+  return UINT128 (msp, lsp);
+}
+
+UINT128 operator- (const UINT128 u, const UINT64 minusee) {
+  UINT64 msp, lsp;
+  asm (
+        "SUB %[a], %%rbx\n\t"
+        "SBB $0, %%rcx"
+        : "=c" (msp), "=b" (lsp)
+        : "c" (u.high), "b" (u.low), [a] "r" (minusee)
+        );
+  return UINT128 (msp, lsp);
+}
+
+UINT128 operator- (const UINT128 u, const UINT128 minusee) {
+  UINT64 msp, lsp;
+  asm (
+        "SUB %[l], %%rbx\n\t"
+        "SBB %[h], %%rcx"
+        : "=c" (msp), "=b" (lsp)
+        : "c" (u.high), "b" (u.low), [h] "r" (minusee.high), [l] "r" (minusee.low)
+        );
+  return UINT128 (msp, lsp);
+}
+
+UINT128 operator- (const UINT128 u) {
+  return UINT128 (0, 0) - u;
+}
+
+UINT64 operator/ (const UINT128 u, const UINT64 divisor) {
+  UINT64 quotient;
+  asm (
+        "DIV %[divisor]"
+        : "=a" (quotient)
+        : "d" (u.high), "a" (u.low), [divisor] "r" (divisor)
+        );
+  return quotient;
+}
+
+UINT64 operator% (const UINT128 u, const UINT64 divisor) {
+  UINT64 remainder;
+  asm (
+        "DIV %[divisor]"
+        : "=d" (remainder)
+        : "d" (u.high), "a" (u.low), [divisor] "r" (divisor)
+        );
+  return remainder;
+}
+
+UINT64 operator% (const UINT64 u, const UINT128 divisor) {
+  if (divisor.high == 0) {
+    return u % (divisor.low);
+  } else { // u < divisor
+    return u;
+  }
+}
+
+UINT128 operator% (const UINT128 u, const UINT128 divisor) {
+  if (divisor.high == 0) {
+    return UINT128 (0, u % (divisor.low));
+  }
+  UINT128 remainder = u;
+  while (remainder >= divisor) {
+    remainder = remainder - divisor;
+  }
+  return remainder;
+}
+
+UINT128 arithmeticShiftToRight (const UINT128 v, const int bits) {
+  return UINT128 (INT64 (v.high) >> bits, (v.high << (64 - bits)) | (v.low >> bits));
+}
+
+bool operator== (const UINT64 a, const UINT128 b) {
   return b == a;
 }
 
-static inline bool operator!= (const UINT64 &a, const UINT128 &b) {
+bool operator!= (const UINT64 a, const UINT128 b) {
   return b != a;
 }
 
-static inline bool operator< (const UINT64 &a, const UINT128 &b) {
+bool operator< (const UINT64 a, const UINT128 b) {
   return b > a;
 }
 
-static inline bool operator<= (const UINT64 &a, const UINT128 &b) {
+bool operator<= (const UINT64 a, const UINT128 b) {
   return b >= a;
 }
 
-static inline bool operator> (const UINT64 &a, const UINT128 &b) {
+bool operator> (const UINT64 a, const UINT128 b) {
   return b < a;
 }
 
-static inline bool operator>= (const UINT64 &a, const UINT128 &b) {
+bool operator>= (const UINT64 a, const UINT128 b) {
   return b <= a;
 }
 
-static inline UINT128 operator+ (UINT64 a, UINT128 b) {
+UINT128 operator+ (const UINT64 a, const UINT128 b) {
   return b + a;
 }
 
-static inline UINT128 operator- (UINT64 a, UINT128 b) {
+UINT128 operator- (const UINT64 a, const UINT128 b) {
   return UINT128 (0, a) - b;
 }
 
-static inline UINT128 logicalShiftToLeft (const UINT128 v, const int bits) {
+UINT128 logicalShiftToLeft (const UINT128 v, const int bits) {
   return v << bits;
 }
 
-static inline UINT128 logicalShiftToLeft (const UINT64 v, const int bits) {
+UINT128 logicalShiftToLeft (const UINT64 v, const int bits) {
   return UINT128 (v >> (64 - bits), v << bits);
 }
 
-static inline UINT64 arithmeticShiftToRight (const UINT64 &v, int bits) {
+UINT64 arithmeticShiftToRight (const UINT64 v, const int bits) {
   return (INT64 (v) >> bits);
 }
 
