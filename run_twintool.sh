d7999f0566fea65d5a1180fb756a95f5aa3cb672
Behnam Momeni <b_momeni@ce.sharif.edu>
2015-12-22 18:12:49 +0330

    Updating the runner script

--- run_twintool.sh
+++ run_twintool.sh
@@ -1,1 +1,3 @@
-sudo su sandbox -c "../../../pin.sh -t ./obj-intel64/TwinTool.so -symbols /tmp/twinner/symbols.dat -trace /tmp/twinner/trace.dat -verbose $1 -- ./obj-intel64/upx-test.upx-packed.out 475"
+cmd="../../../pin.sh -t ./obj-intel64/TwinTool.so -symbols /tmp/twinner/symbols.dat -trace /tmp/twinner/trace.dat -memory /tmp/twinner/memory.dat -verbose $@ -main -mar /tmp/twinner/main-args-reporting.dat -- ./obj-intel64/mpwpl-manual-virt-obf.out 475"
+echo "Running \"$cmd\""
+sudo su sandbox -c "$cmd"
