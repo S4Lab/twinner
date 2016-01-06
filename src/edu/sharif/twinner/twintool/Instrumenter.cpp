7663cf2f5e73d42cd6f4cf8ade3ab48828f478af
Behnam Momeni <b_momeni@ce.sharif.edu>
2016-01-06 22:04:15 +0330

    Implementing INITIALIZE macro in windows without sizeof magic

--- src/edu/sharif/twinner/twintool/Instrumenter.cpp
+++ src/edu/sharif/twinner/twintool/Instrumenter.cpp
@@ -78,9 +78,22 @@ Instrumenter::Instrumenter (const string &_traceFilePath,
   initialize ();
 }
 
+#ifdef TARGET_LINUX
 #define INITIALIZE(MODEL, OPCODES...) \
   initialize<sizeof ((const OPCODE[]) {OPCODES}) / sizeof (OPCODE)> \
     (MODEL, (const OPCODE[]) {OPCODES})
+#endif
+#ifdef TARGET_WINDOWS
+#define Y_TUPLE_SIZE(...) Y_TUPLE_SIZE_II((Y_TUPLE_SIZE_PREFIX_ ## __VA_ARGS__ ## _Y_TUPLE_SIZE_POSTFIX,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0))
+#define Y_TUPLE_SIZE_II(__args) Y_TUPLE_SIZE_I __args
+#define Y_TUPLE_SIZE_PREFIX__Y_TUPLE_SIZE_POSTFIX ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,0
+#define Y_TUPLE_SIZE_I(__p0,__p1,__p2,__p3,__p4,__p5,__p6,__p7,__p8,__p9,__p10,__p11,__p12,__p13,__p14,__p15,__p16,__p17,__p18,__p19,__p20,__p21,__p22,__p23,__p24,__p25,__p26,__p27,__p28,__p29,__p30,__p31,__n,...) __n
+#define INITIALIZE(MODEL, ...) \
+  do { \
+    const OPCODE opcodes[] = {__VA_ARGS__}; \
+    initialize<Y_TUPLE_SIZE (__VA_ARGS__)> (MODEL, opcodes); \
+  } while (false)
+#endif
 
 void Instrumenter::initialize () {
   edu::sharif::twinner::util::Logger::info ()
