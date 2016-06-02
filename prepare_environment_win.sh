ca358e4764ace9eaaa0bb6a3fb104efd214affc3
Behnam Momeni <b_momeni@ce.sharif.edu>
2016-06-02 22:17:38 +0330

    Updating win scripts

--- prepare_environment_win.sh
+++ prepare_environment_win.sh
@@ -5,9 +5,13 @@ cp -f -l -a -u mem.sh run_twinner_win.sh run_paused_twintool.sh gdb_for_pintool.
 cd deploy/
 rm depends.sh
 mv depends_win.sh depends.sh
+mkdir -p obj-ia32
 cd ..
 #cp -f -l -a -u "test/simple-condition-test.out" "test/multi-path-with-printf-leafs-test.out" "test/mpwpl-manual-virt-obf.out" "deploy/obj-ia32/"
 cp -f -l -a -u "test/mpwpl-manual-virt-obf.out" "deploy/obj-ia32/"
+if [ ! -f deploy/obj-ia32/libcvc4.dll ]; then
+  cp -l /usr/local/lib/libcvc4.dll deploy/obj-ia32/
+fi
 
 if [ "a$fullpreparation" = "afull" ]; then
 	echo "Full preparation..."
