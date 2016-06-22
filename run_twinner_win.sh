1ea0f6d69a2a57906089049ec4415f3a4f9fa864
Behnam Momeni <b_momeni@ce.sharif.edu>
2016-06-22 11:42:28 +0430

    Adjusting endpoints and stack offset in runner scripts

--- run_twinner_win.sh
+++ run_twinner_win.sh
@@ -1,2 +1,2 @@
 rm -f /tmp/twinner/*
-./obj-ia32/Twinner --input ./obj-ia32/mpwpl-manual-virt-obf.out --args '475' --pin-launcher ../../../pin.exe --tool ./obj-ia32/TwinTool.dll -o /tmp/twinner/unpacked.twin.c --safe-functions 'printf@0x401614#auto' --analysis-endpoints 0x4012d9,0x40135d --verbose $@
+./obj-ia32/Twinner --input ./obj-ia32/mpwpl-manual-virt-obf.out --args '475' --pin-launcher ../../../pin.exe --tool ./obj-ia32/TwinTool.dll -o /tmp/twinner/unpacked.twin.c --safe-functions 'printf@0x401614#auto' --analysis-endpoints 0x4012e7,0x40135d --stack-offset 11 --verbose $@
--- run_twintool_win.sh
+++ run_twintool_win.sh
@@ -1,3 +1,3 @@
-cmd="../../../pin.exe -pin_memory_range 0x40000000:0x60000000 -t ./obj-ia32/TwinTool.dll -symbols /tmp/twinner/symbols.dat -trace /tmp/twinner/trace.dat -memory /tmp/twinner/memory.dat -verbose $@ -endpoints 0x4012d9,0x40135d -mar /tmp/twinner/main-args-reporting.dat -safe-functions printf@0x401614#auto -- ./obj-ia32/mpwpl-manual-virt-obf.out 475"
+cmd="../../../pin.exe -pin_memory_range 0x40000000:0x60000000 -t ./obj-ia32/TwinTool.dll -symbols /tmp/twinner/symbols.dat -trace /tmp/twinner/trace.dat -memory /tmp/twinner/memory.dat -verbose $@ -endpoints 0x4012e7,0x40135d -stack-offset 11 -mar /tmp/twinner/main-args-reporting.dat -safe-functions printf@0x401614#auto -- ./obj-ia32/mpwpl-manual-virt-obf.out 475"
 echo "Running \"$cmd\""
 $cmd
