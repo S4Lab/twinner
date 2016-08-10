f10535ca7f59173a5319cf7a2cdfedde389f39ba
Behnam Momeni <b_momeni@ce.sharif.edu>
2016-08-10 20:11:38 +0330

    Updating the twintool runner script for vmp target

--- run_twintool_win.sh
+++ run_twintool_win.sh
@@ -1,3 +1,3 @@
-cmd="../../../pin.exe -pin_memory_range 0x40000000:0x60000000 -t ./obj-ia32/TwinTool.dll -symbols tmp/twinner/symbols.dat -trace tmp/twinner/trace.dat -memory tmp/twinner/memory.dat -verbose $@ -logfilename out-twintool -endpoints 0x4012e7,0x40135d -stack-offset 11 -mar tmp/twinner/main-args-reporting.dat -safe-functions printf@0x401614#auto -- ./obj-ia32/mpwpl-manual-virt-obf.out 475"
-echo "Running \"$cmd\""
-$cmd
+args="-pin_memory_range 0x40000000:0x60000000 -t ./obj-ia32/TwinTool.dll -symbols tmp/twinner/symbols.dat -trace tmp/twinner/trace.dat -memory tmp/twinner/memory.dat -verbose $@ -logfilename out-twintool -endpoints 0x4012d9,0x4012ef -stack-offset 11 -mar tmp/twinner/main-args-reporting.dat -safe-functions printf@0x401308#auto,puts@0x401310#auto -- ./obj-ia32/multi-path-with-printf-leafs-test-annotated.vmp.exe 123"
+echo "Running \"../../../pin.exe $args\""
+../../../pin.exe $args
