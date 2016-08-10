9aa4a7983231ee0a3fedf12a75d6b7310102df47
Behnam Momeni <b_momeni@ce.sharif.edu>
2016-08-10 10:57:04 +0330

    Specifying test case analysis endpoints

---
+++ test/multi-path-with-printf-leafs-test-annotated.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main_func (int argc, char *argv[]) {
+	if (argc < 2) {
+		printf ("Usage: program <command codes>\n");
+		return -1;
+	}
+	int a = argv[1][0] - '0';
+	int b = argv[1][1] - '0';
+	int c = argv[1][2] - '0';
+	if (a > 5) {
+		if (b > 5) {
+			if (c > 5) {
+				printf ("a > 5, b > 5, c > 5 -- case 1\n");
+			} else {
+				printf ("a > 5, b > 5, c <= 5 -- case 2\n");
+			}
+		} else if (c > 5) {
+			printf ("a > 5, b <= 5, c > 5 -- case 3\n");
+		} else {
+			printf ("a > 5, b <= 5, c <= 5 -- case 4\n");
+		}
+	} else if (b > 5) {
+		if (c > 5) {
+			printf ("a <= 5, b > 5, c > 5 -- case 5\n");
+		} else {
+			printf ("a <= 5, b > 5, c <= 5 -- case 6\n");
+		}
+	} else {
+		if (c > 5) {
+			printf ("a <= 5, b <= 5, c > 5 -- case 7\n");
+		} else {
+			printf ("a <= 5, b <= 5, c <= 5 -- case 8\n");
+		}
+	}
+	printf ("multi-path program with printf-leafs\n");
+	return 0;
+}
+
+int main (int argc, char *argv[]) {
+	int res;
+	printf ("endpoints: 0x%x, 0x%x\n", &&start, &&end);
+start:
+	res = main_func (argc, argv);
+end:
+	return res;
+}
+
