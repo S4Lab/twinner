792f1f073f0ba48bf5d904468c9e292f8fbc2857
Behnam Momeni <b_momeni@ce.sharif.edu>
2017-01-11 13:25:30 +0330

    Updating runner scripts; first arg is new/replay, second is verboseness

--- run_twintool_win.sh
+++ run_twintool_win.sh
@@ -1,3 +1,3 @@
-args="-pin_memory_range 0x40000000:0x60000000 -t ./obj-ia32/TwinTool.dll -symbols tmp/twinner/symbols.dat -trace tmp/twinner/trace.dat -memory tmp/twinner/memory.dat -verbose $@ -logfilename out-twintool -endpoints 0x4012d9,0x4012ef -stack-offset 11 -mar tmp/twinner/main-args-reporting.dat -safe-functions printf@0x401308#auto,puts@0x401310#auto -- ./obj-ia32/multi-path-with-printf-leafs-test-annotated.vmp.exe 123"
+args="-pin_memory_range 0x40000000:0x60000000 -t ./obj-ia32/TwinTool.dll -symbols tmp/twinner/symbols.dat -trace tmp/twinner/trace.dat -memory tmp/twinner/memory.dat -verbose $@ -logfilename out-twintool -endpoints 0x40124c,0x4012c2 -stack-offset 11 -mar tmp/twinner/main-args-reporting.dat -safe-functions printf@0x4015e4#auto -- ./obj-ia32/input1.vo.out 916"
 echo "Running \"../../../pin.exe $args\""
 ../../../pin.exe $args
