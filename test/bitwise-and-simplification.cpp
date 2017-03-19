e66aeaccb7420ea8e1fcfc9a1f7bc8cf222ba685
Behnam Momeni <b_momeni@ce.sharif.edu>
2017-03-19 16:44:59 +0330

    Adding a test case for truncation simplification

--- test/bitwise-and-simplification.cpp
+++ test/bitwise-and-simplification.cpp
@@ -21,11 +21,13 @@ using namespace edu::sharif::twinner::util;
 
 
 void test1 ();
+void test2 ();
 
 int main (int argc, char *argv[]) {
   LogStream::init ("loquacious", "out-test-bitwise-and");
   MarInfo (argc, argv);
   test1 ();
+  test2 ();
   LogStream::destroy ();
   return 0;
 }
@@ -39,5 +41,14 @@ void test1 () {
   exp->bitwiseXor (0x100);
   exp->bitwiseAnd (0xffffffff);
   Logger::debug () << "exp: " << exp << '\n';
+}
+
+void test2 () {
+  Logger::info () << "test 2: simplifying 0x7ffff4be8498 & 0xffffffff\n";
+  Expression *orig = new ExpressionImp (UINT64 (0x7ffff4be8498));
+  Expression *exp = orig->clone (32);
+  Logger::loquacious () << "exp: " << exp->toDetailedString () << '\n';
+  exp->truncate (32);
+  Logger::debug () << "exp: " << exp->toDetailedString () << '\n';
 }
 
