d3860ce86afe437ababebc753a0cac713d4c7bf7
Behnam Momeni <b_momeni@ce.sharif.edu>
2017-08-08 21:14:15 +0430

    Updating scripts for input1

--- run_twinner.sh
+++ run_twinner.sh
@@ -1,4 +1,4 @@
-sudo su sandbox -c "rm -f /tmp/twinner/*"
-sudo su sandbox -c "./obj-intel64/Twinner --input ./obj-intel64/mpwpl-manual-virt-obf.out --args '475' --pin-launcher ../../../pin.sh --tool ./obj-intel64/TwinTool.so -o /tmp/twinner/unpacked.twin.c --safe-functions 'printf@0x4005b0#auto' --main --verbose $@"
+#sudo su sandbox -c "rm -f ./tmp/twinner/*"
+sudo su sandbox -c "./obj-intel64/Twinner --input ./obj-intel64/input1.out --args '475' --pin-launcher ../../../pin.sh --tool ./obj-intel64/TwinTool.so -o ./tmp/twinner/unpacked.twin.c --tmpfolder ./tmp --safe-functions 'puts@0x400410#auto' --generate-etg ./tmp/twinner/etg.dot --main --verbose $@"
 #sudo su sandbox -c "./obj-intel64/Twinner --input ./obj-intel64/runtime-simplification.out --args '2' --pin-launcher ../../../pin.sh --tool ./obj-intel64/TwinTool.so -o /tmp/twinner/unpacked.twin.c --safe-functions 'printf@0x400460#auto,puts@0x400450#1{const char *}' --main --verbose $@"
 #sudo su sandbox -c "./obj-intel64/Twinner --input ./obj-intel64/runtime-simplification.out --args '2' --pin-launcher ../../../pin.sh --tool ./obj-intel64/TwinTool.so -o /tmp/twinner/unpacked.twin.c --safe-functions 'printf@0x400460#auto,puts@0x400450#auto' --main --verbose $@"
--- run_twintool.sh
+++ run_twintool.sh
@@ -1,3 +1,3 @@
-cmd="../../../pin.sh -t ./obj-intel64/TwinTool.so -symbols /tmp/twinner/symbols.dat -trace /tmp/twinner/trace.dat -memory /tmp/twinner/memory.dat -verbose $@ -main -mar /tmp/twinner/main-args-reporting.dat -- ./obj-intel64/mpwpl-manual-virt-obf.out 475"
+cmd="../../../pin.sh -pin_memory_range 0x40000000:0x60000000 -t ./obj-intel64/TwinTool.so -symbols ./tmp/twinner/symbols.dat -trace ./tmp/twinner/trace.dat -memory ./tmp/twinner/memory.dat -verbose $@ -logfilename out-input1-twintool -main -mar ./tmp/twinner/main-args-reporting.dat -safe-functions 'puts@0x400410#auto' -- ./obj-intel64/input1.out 475"
 echo "Running \"$cmd\""
 sudo su sandbox -c "$cmd"
